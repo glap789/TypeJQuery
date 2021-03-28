995403a10e450f2c8b59eca2fc731a1aa87c42ac
Heber <glap789@gmail.com>
2021-03-28 11:46:24 -0500

    el rico commit

---
+++ main.js
@@ -0,0 +1,30 @@
+
+    $('#user').on('keypress', function(){
+
+        var text = $('#user').val();
+         $('.usuario').html(text)
+    })
+
+
+    $('#foto').on('change', function(e){
+
+
+        let reader = new FileReader();
+    
+        
+        reader.readAsDataURL(e.target.files[0]);
+    
+       
+        reader.onload = function() {
+            let preview = document.getElementById('fotito'),
+                image = document.createElement('img');
+    
+            image.src = reader.result;
+    
+            preview.innerHTML = '';
+            preview.append(image);
+        };
+
+
+    })
+
