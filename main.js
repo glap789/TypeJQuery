051c3c6b41efe26baa6570e64cf0a0b1df12216f
Heber <glap789@gmail.com>
2021-03-28 12:39:43 -0500

    commit4

--- main.js
+++ main.js
@@ -33,7 +33,18 @@
 
         var x = $('#x').val()
         var y = $('#y').val()
+        var canvas_t = 0
+        var canvas_b = 500
+        var canvas_l = 0
+        var canvas_r = 500
+       
+        if(x > canvas_t && x < canvas_b && x > canvas_l && x < canvas_r && y > canvas_t && y < canvas_b && y > canvas_l && y < canvas_r)
+        {
+            $("#fotito").offset({top: x, left: y});
+        }
+        else{alert("fuera de rango")}
 
-        $("#fotito").offset({top: x, left: y});
+       
+        
     });
 
