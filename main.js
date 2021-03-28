a4cc9f43dd70ce9ba9ccffbc39896ae78cb6e60d
Heber <glap789@gmail.com>
2021-03-28 13:02:41 -0500

    commit 5

--- main.js
+++ main.js
@@ -34,9 +34,9 @@
         var x = $('#x').val()
         var y = $('#y').val()
         var canvas_t = 0
-        var canvas_b = 500
+        var canvas_b = 700
         var canvas_l = 0
-        var canvas_r = 500
+        var canvas_r = 700
        
         if(x > canvas_t && x < canvas_b && x > canvas_l && x < canvas_r && y > canvas_t && y < canvas_b && y > canvas_l && y < canvas_r)
         {
